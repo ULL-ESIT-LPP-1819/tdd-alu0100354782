18188ce575dc6a324535dfefc9e3093f950f752e
alu0100354782 <alu0100354782@ull.edu.es>
2018-11-09 11:56:45 +0000

    Error clase InfoNutricional no tiene constructor que espera nombre

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -3,4 +3,10 @@ RSpec.describe Prct06 do
     expect(Prct06::VERSION).not_to be nil
   end
 
+  describe InfoNutricional do
+	before :each do
+	  @info = InfoNutricional.new("nombre")
+	end
+  end
+
 end
