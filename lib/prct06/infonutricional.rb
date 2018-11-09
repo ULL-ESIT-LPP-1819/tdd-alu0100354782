85f60a687be4d741e65ce9a8b2a403f3d4546313
alu0100354782 <alu0100354782@ull.edu.es>
2018-11-09 13:24:17 +0000

    Prueba Corregida - Debe existir la candidad de azucares

--- lib/prct06/infonutricional.rb
+++ lib/prct06/infonutricional.rb
@@ -3,13 +3,14 @@
 
 class InfoNutricional
 
-	attr_reader :nombre_, :grasas_, :grasas_saturadas_, :hidratos_
+	attr_reader :nombre_, :grasas_, :grasas_saturadas_, :hidratos_, :azucares_
 
-	def initialize(nombre, grasas, grasas_saturadas, hidratos)
+	def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares)
 		@nombre_ = nombre
 		@grasas_ = grasas
 		@grasas_saturadas_ = grasas_saturadas
 		@hidratos_ = hidratos
+		@azucares_ = azucares
 	end
 end
 
