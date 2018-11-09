5b8a35318c228d13b0f812dd19b926f0ab62f6ae
alu0100354782 <alu0100354782@ull.edu.es>
2018-11-09 13:27:56 +0000

    Prueba Corregida - Debe existir la candidad de proteínas

--- lib/prct06/infonutricional.rb
+++ lib/prct06/infonutricional.rb
@@ -3,14 +3,15 @@
 
 class InfoNutricional
 
-	attr_reader :nombre_, :grasas_, :grasas_saturadas_, :hidratos_, :azucares_
+	attr_reader :nombre_, :grasas_, :grasas_saturadas_, :hidratos_, :azucares_, :proteinas_
 
-	def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares)
+	def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas)
 		@nombre_ = nombre
 		@grasas_ = grasas
 		@grasas_saturadas_ = grasas_saturadas
 		@hidratos_ = hidratos
 		@azucares_ = azucares
+		@proteinas_ = proteinas
 	end
 end
 
