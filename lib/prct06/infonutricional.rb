bda2321b094748bc34fcfaacb6c179e08ed7a6f2
alu0100354782 <alu0100354782@ull.edu.es>
2018-11-09 13:32:44 +0000

    Prueba Corregida - Debe existir la candidad de sal

--- lib/prct06/infonutricional.rb
+++ lib/prct06/infonutricional.rb
@@ -3,15 +3,16 @@
 
 class InfoNutricional
 
-	attr_reader :nombre_, :grasas_, :grasas_saturadas_, :hidratos_, :azucares_, :proteinas_
+	attr_reader :nombre_, :grasas_, :grasas_saturadas_, :hidratos_, :azucares_, :proteinas_, :sal_
 
-	def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas)
+	def initialize(nombre, grasas, grasas_saturadas, hidratos, azucares, proteinas, sal)
 		@nombre_ = nombre
 		@grasas_ = grasas
 		@grasas_saturadas_ = grasas_saturadas
 		@hidratos_ = hidratos
 		@azucares_ = azucares
 		@proteinas_ = proteinas
+		@sal_ = sal
 	end
 end
 
