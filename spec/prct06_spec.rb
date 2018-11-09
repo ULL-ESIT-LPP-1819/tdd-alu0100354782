b11b03222427630c4620e860ad0ff4c59550ee89
alu0100354782 <alu0100354782@ull.edu.es>
2018-11-09 12:57:43 +0000

    Error Prueba Debe existir la candidad de grasas

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -3,14 +3,22 @@ RSpec.describe Prct06 do
 		expect(Prct06::VERSION).not_to be nil
 	end
 
+	#it "does something useful" do
+    #	expect(true).to eq(true)
+	#end
+
 
 	before :each do
-		@etiqueta = InfoNutricional.new("Pan")
+		@etiqueta = InfoNutricional.new("Pan", 70)
 	end
 
 	describe InfoNutricional do
 		it "La etiqueta tiene nombre" do
 			expect(@etiqueta.nombre_).to eq("Pan")
+		end
+		
+		it "Debe existir la candidad de grasas" do
+			expect(@etiqueta.grasas_).to eq(70)
 		end
 	end
 
