fff911bca090009b31b01343d55431e1746b56ad
alu0100354782 <alu0100354782@ull.edu.es>
2018-11-15 13:37:51 +0000

    Correcciones en los mensajes de las pruebas

--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -29,11 +29,11 @@ RSpec.describe Prct06 do
 			expect(@etiqueta.hidratos_).to eq(260)
 		end
 
-		it "Debe existir la candidad de azucares" do
+		it "Debe existir la candidad de azúcares" do
 			expect(@etiqueta.azucares_).to eq(90)
 		end
 
-		it "Debe existir la candidad de proteınas" do
+		it "Debe existir la candidad de proteínas" do
 			expect(@etiqueta.proteinas_).to eq(50)
 		end
 
@@ -41,15 +41,15 @@ RSpec.describe Prct06 do
 			expect(@etiqueta.sal_).to eq(6)
 		end
 
-		it "Existe un metodo para obtener el nombre" do
+		it "Existe un método para obtener el nombre" do
 			expect(@etiqueta.get_nombre).to eq("Pan")
 		end
 
-		it "Existe un metodo para obtener el valor energetico" do
+		it "Existe un método para obtener el valor energético" do
 			expect(@etiqueta.get_val_energetico).to eq(8400)
 		end
 
-		it "Existe un m ́etodo para obtener la cantidad de grasas" do
+		it "Existe un método para obtener la cantidad de grasas" do
 			expect(@etiqueta.get_grasas).to eq(70)
 		end
 	end
