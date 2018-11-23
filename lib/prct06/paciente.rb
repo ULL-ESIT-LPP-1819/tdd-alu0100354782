95c49302c4323d7895293b6a39e21d29eb67bf04
alu0100354782 <alu0100354782@ull.edu.es>
2018-11-23 05:25:26 +0000

    Añadida clase Paciente hija de Persona y con atributo tipo Antropometría

---
+++ lib/prct06/paciente.rb
@@ -0,0 +1,20 @@
+require 'pry'
+require 'Persona'
+
+class Paciente < Persona
+    
+    attr_accessor :consulta, :tratamiento, :antropometria
+
+    def initialize(nombre, apellido)
+        super(nombre,apellido)
+    end
+
+    def initialize(nombre, apellido, consulta, tratamiento, antropometria)
+        super(nombre, apellido)
+        @consulta = consulta
+        @tratamiento = tratamiento
+        @antropometria = antropometria
+    end
+    
+end
+binding.pry
